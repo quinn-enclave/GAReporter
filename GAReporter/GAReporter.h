dcb643ff1db3687f06aeb415815b1673d3c5eb4a
Quinn Nguyen <quinn@enclave.vn>
2020-06-18 15:19:33 +0700

    Initial Commit

---
+++ GAReporter/GAReporter.h
@@ -0,0 +1,19 @@
+//
+//  GAReporter.h
+//  GAReporter
+//
+//  Created by Quinn on 6/18/20.
+//  Copyright © 2020 Quinn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GAReporter.
+FOUNDATION_EXPORT double GAReporterVersionNumber;
+
+//! Project version string for GAReporter.
+FOUNDATION_EXPORT const unsigned char GAReporterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GAReporter/PublicHeader.h>
+
+
